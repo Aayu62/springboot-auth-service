5b1f3409248594301e11fdcab64b581fa681f035
Aayu62 <ayushbarnwal062@gmail.com>
2026-05-17 16:03:06 +0530

    Added UserRepository for database operations using Spring Data JPA

---
+++ src/main/java/com/ayush/authservice/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ayush.authservice.repository;
+
+import java.util.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ayush.authservice.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findbyEmail(String email);
+    
+}

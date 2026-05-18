763238c46dab9a1d5ae27a5f652488b70e68d41f
Aayu62 <ayushbarnwal062@gmail.com>
2026-05-18 18:02:44 +0530

    Added AuthService for User Registration Business Logic

---
+++ src/main/java/com/ayush/authservice/service/AuthService.java
@@ -0,0 +1,29 @@
+package com.ayush.authservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ayush.authservice.entity.User;
+import com.ayush.authservice.dto.RegisterRequest;
+import com.ayush.authservice.repository.UserRepository;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+    
+    public String register(RegisterRequest request){
+
+        User user = new User(
+            request.getName(),
+            request.getEmail(),
+            request.getPassword()
+        );
+
+        userRepository.save(user);
+
+        return "User Registered Successfully";
+
+    }
+}
